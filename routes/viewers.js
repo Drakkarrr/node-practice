f36280b5a702e4114c94943638d54f1ecfb16fed
Junrey Lañas <jrydev404@gmail.com>
2022-09-02 12:52:17 +0800

    status code

--- routes/viewers.js
+++ routes/viewers.js
@@ -2,35 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/fetch', (req, res) => {
-    res.json({
-        message: 'This is the list of all viewers',
-        viewers: [
-        {
-            id: 1,
-            name: 'John Doe',
-        },
-        {
-            id: 2,
-            name: 'Drakkar Doe',
-        }
-    ]
-    });
+    const { name, age, gender } = req.body;
+    
+    if (Number(age) === 22) {
+        res.status(200).send({
+            name,
+            age,
+            gender
+        });
+    }
+    else {
+        res.status(500).send({
+            "error": "Age is not 22",
+            "status": "error"
+        });
+    }
 })
 
-router.post('/insert', (req, res) => {
-    res.json({
-        message: 'New viewver has been added',
-        viewers: [
-            {
-                id: 3,
-                name: 'Peanut',
-            },
-            {
-                id: 4,
-                name: 'Puso',
-            }
-        ]
-    });
-})
 
 module.exports = router;
